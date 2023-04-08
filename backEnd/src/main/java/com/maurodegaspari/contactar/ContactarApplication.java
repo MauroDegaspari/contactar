2dfe7fbb4baa7220242c1c8d9f2b7ce43efd2436
MauroDegaspari <mauro_degaspari@hotmail.com>
2023-04-08 14:18:43 -0300

    backend - inicialização de projeto backand

---
+++ backEnd/src/main/java/com/maurodegaspari/contactar/ContactarApplication.java
@@ -0,0 +1,13 @@
+package com.maurodegaspari.contactar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactarApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/com/maurodegaspari/contactar/ContactarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maurodegaspari.contactar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
